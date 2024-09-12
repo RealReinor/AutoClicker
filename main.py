0434e4ad875a49bf5b7e4f6005ec842c0eb8b86a
RealReinor <viliampavlovsky08@gmail.com>
2024-09-12 19:43:41 +0200

    Added Gui and Cleanup
    Added gui works with hooked keys still need to make the stop event better.

--- main.py
+++ main.py
@@ -1,163 +1,60 @@
-import keyboard
-import pyautogui
-import json
-import threading
-import pygetwindow as gw
-import mouse
-
-auto_clicking = False
-start_button = ""
-quit_button = ""
-settings_button = ""
-clicking_button = ""
-
-# TO DO LIST
-# Complete Settings
-# Choose want you want to click
-# Menu
-# Make EXE file for startup
-pyautogui.PAUSE = 0.1
+import customtkinter as ctk
+from clicking_manager import ClickingManager
 
 
-def auto_click():
-    global auto_clicking
-    while auto_clicking:
-        pyautogui.click()
-        # keyboard.send("space")
-        # keyboard.send("w")
+class AutoClickerApp:
+    def __init__(self, root_):
+        # ClickingManager Instance
+        self.clicking_manager = ClickingManager()
 
+        # Variables
+        self.click_thread = None
+        self.reading_thread = None
 
-def read_key_event():
-    while True:
-        event = keyboard.read_event(suppress=True)
-        if event.event_type == "up":
-            return event.name
+        # Configure appearance
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("blue")
 
+        # App settings
+        self.root = root_
+        self.root.geometry("320x480")
+        self.root.title("AutoClicker")
 
-def update_settings(key, value):
-    with open("settings.json", "r+") as f:
-        data = json.load(f)
-        data[key] = value
-        f.seek(0)
-        json.dump(data, f, indent=4)
-        f.truncate()
+        # UI elements
+        self.create_widgets()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
+    def create_widgets(self):
+        # Title
+        title = ctk.CTkLabel(self.root, text='Welcome To AutoClicker By "Reinor"', text_color="white", font=("Helvetica", 14, "bold"))
+        title.pack(padx=10, pady=10)
 
-def change_button(current_bt, bt_name):
-    print(f"\nCurrent {bt_name}: {current_bt}")
-    print("Choose what you want to do:")
-    print("1: Change Button")
-    print("2: Go back")
+        # Start Button
+        start_button = ctk.CTkButton(self.root, text="Start", command=self.start_button_on_click)
+        start_button.pack(pady=10)
 
-    action = read_key_event()
+        # Stop Button
+        stop_button = ctk.CTkButton(self.root, text="Stop", command=self.stop_button_on_click)
+        stop_button.pack(pady=10)
 
-    if action == "1":
-        print(f"Please press the new button for {bt_name}")
-        new_key = read_key_event()
-        update_settings(f"{bt_name.lower().replace(' ', '_')}", new_key)
-        print(f"{bt_name} was changed to {new_key}")
-        return new_key
-    return current_bt
+        # Start Reading Keys
+        self.clicking_manager.read_key_event()
 
+    def start_button_on_click(self):
+        if not self.clicking_manager.auto_clicking:
+            self.clicking_manager.start_auto_click()
 
-def change_clicking_button(current_bt):
-    print(f"\nCurrent Clicking Button: {current_bt}")
-    print("Choose what you want to do:")
-    print("1: Change Button")
-    print("2: Go back")
+    def stop_button_on_click(self):
+        if self.clicking_manager.auto_clicking:
+            self.clicking_manager.stop_auto_click()
 
-    action = read_key_event()
-
-    if action == "1":
-        print("\nFor clicking button do you use mouse or keyboard")
-        print("1: Mouse")
-        print("2: Keyboard")
-
-        method = read_key_event()
-
-        if method == "1":
-            print("\nPress either the left mouse button or the right mouse button.")
-            while True:
-                if mouse.is_pressed("left"):
-                    new_button = "l_button"
-                    break
-                elif mouse.is_pressed("right"):
-                    new_button = "r_button"
-                    break
-            update_settings("clicking_button", new_button)
-            print(f"Clicking Button was changed to {new_button}")
-            return new_button
-    return current_bt
-
-
-def settings(start_bt, quit_bt, settings_bt, clicking_bt):
-    global start_button, quit_button, settings_button, clicking_button
-
-    while True:
-        print("\nChoose which button you want to change:")
-        print("1: Start Button")
-        print("2: Quit Button")
-        print("3: Settings Button")
-        print("4: Change Clicking Button")
-        print("5: Exit")
-
-        choice = read_key_event()
-
-        if choice == "1":
-            start_button = change_button(start_bt, "Start Button")
-        elif choice == "2":
-            quit_button = change_button(quit_bt, "Quit Button")
-        elif choice == "3":
-            settings_button = change_button(settings_bt, "Settings Button")
-        elif choice == "4":
-            clicking_button = change_clicking_button(clicking_bt)
-        elif choice == "5":
-            print("\nExiting...")
-            print(
-                f"\nStart AutoClicker by pressing \"{start_button.upper()}\" or exit by pressing \"{quit_button.upper()}\"")
-            print(f"Open settings by pressing \"{settings_button.upper()}\"")
-            break
-
-
-def main():
-    with open("info.json") as i, open("settings.json") as s:
-        global auto_clicking, start_button,quit_button,settings_button,clicking_button
-
-        info = json.load(i)
-        settings_json = json.load(s)
-
-        start_button = settings_json["start_button"]
-        quit_button = settings_json["quit_button"]
-        settings_button = settings_json["settings_button"]
-        clicking_button = settings_json["clicking_button"]
-
-        print(f"Welcome to \"Anime\" AutoClicker {info["version"]} by {"".join(info["author"])}")
-        print(f"\nStart AutoClicker by pressing \"{start_button.upper()}\" or exit by pressing \""f"{quit_button.upper()}\"")
-        print(f"Open settings by pressing \"{settings_button.upper()}\"")
-
-        script_window = gw.getActiveWindow()
-
-        while True:
-            event = keyboard.read_event()
-            if event.name == quit_button and event.event_type == "up":
-                if auto_clicking:
-                    print("Please stop auto clicking before exiting!")
-                else:
-                    print("\nEnding AutoClicker")
-                    break
-            elif event.name == start_button and event.event_type == "up":
-                if not auto_clicking:
-                    auto_clicking = True
-                    t1 = threading.Thread(target=auto_click)
-                    t1.start()
-                    continue
-                else:
-                    auto_clicking = False
-                    t1.join()
-            elif event.name == settings_button and event.event_type == "up":
-                if gw.getActiveWindow() == script_window:
-                    settings(start_button, quit_button, settings_button,clicking_button)
+    def on_close(self):
+        self.clicking_manager.stop_auto_click()
+        self.clicking_manager.stop_key_listener()
+        root.destroy()
 
 
 if __name__ == '__main__':
-    main()
+    root = ctk.CTk()
+    AutoClickerApp(root)
+    root.mainloop()
